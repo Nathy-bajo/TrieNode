9e08e349f3f240873a22320c1044f3dfdef53033
Nathy-bajo <bajon7680@gmail.com>
2023-10-09 13:29:23 +0100

    Tests added

--- src/main.rs
+++ src/main.rs
@@ -45,16 +45,21 @@ impl Trie {
     }
 
     pub fn starts_with(&self, prefix: &str) -> bool {
+        if prefix.is_empty() {
+            return true;
+        }
+    
         let mut current = &self.root;
         for ch in prefix.chars() {
             let index = (ch as u8 - b'a') as usize;
             if current.children[index].is_none() {
-                return false;
+                return false; 
             }
             current = current.children[index].as_ref().unwrap();
         }
         true
     }
+    
 }
 
 fn main() {
@@ -68,4 +73,43 @@ fn main() {
 }
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn test_insert_and_search() {
+        let mut trie = Trie::new();
+
+        trie.insert("apple");
+        trie.insert("banana");
+        trie.insert("cherry");
+
+        assert!(trie.search("apple"));
+        assert!(trie.search("banana"));
+        assert!(trie.search("cherry"));
+
+        assert!(!trie.search("grape"));
+        assert!(!trie.search("orange"));
+        assert!(!trie.search("strawberry"));
+    }
+
+    #[test]
+    fn test_starts_with() {
+        let mut trie = Trie::new();
+    
+        trie.insert("apple");
+        trie.insert("banana");
+        trie.insert("cherry");
+    
+        assert!(trie.starts_with("a"));
+        assert!(trie.starts_with("app"));
+        assert!(trie.starts_with("banan")); 
+        assert!(trie.starts_with("cher"));
+        assert!(!trie.starts_with("gr"));
+    
+        let empty_trie = Trie::new();
+        assert!(!empty_trie.starts_with("a"));
+    }
+    
+}
